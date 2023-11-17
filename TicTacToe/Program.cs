0545454647dbb1f29ff7149facf841f8911c2676
Chocketa Chok <jonatanlakes1998@gmail.com>
2023-11-18 00:26:53 +0200

    Improved

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TicTacToe
@@ -14,18 +15,20 @@ namespace TicTacToe
             int numOfPlays = 0;
             char player;
             int index;
+            int cycle = 0;
+         
 
             Console.WriteLine("  1   2   3");
-            Console.WriteLine("| {0} | {1} | {2} |  a", board[0], board[1], board[2]);
+            Console.WriteLine("| ? | ? | ? |  a");
             Console.WriteLine(" ------------");
-            Console.WriteLine("| {0} | {1} | {2} |  b", board[3], board[4], board[5]);
+            Console.WriteLine("| ? | ? | ? |  b");
             Console.WriteLine(" ------------");
-            Console.WriteLine("| {0} | {1} | {2} |  c", board[6], board[7], board[8]);
+            Console.WriteLine("| ? | ? | ? |  c");
             Console.WriteLine();
 
-            while (true)
+            while (cycle <= 9)
             {
-                if (numOfPlays % 2 != 0)
+                if (numOfPlays % 2 == 0)
                 {
                     Console.WriteLine("You're playing as X");
                     player = 'X';
@@ -36,55 +39,46 @@ namespace TicTacToe
                 }
                 Console.WriteLine("Input column: (1,2,3)");
                 int column = int.Parse(Console.ReadLine());
+                if (column < 1 || column > 3)
+                {
+                    TryAgain(cycle, board);
+                    continue;
+                }
+
                 Console.WriteLine("Input row: (a, b, c)");
                 char row = char.Parse(Console.ReadLine());
                 index = (int)row - '0' - '0';
-                if (row == 'b') index += column;
+                if (row < 'a' || row > 'c')
+                {
+                    TryAgain(cycle, board);
+                    continue;
+
+                }
+
+                else if (row == 'b') index += column;
                 else if (row == 'c') index += (column + 2);
                 else index = Math.Abs(index - column);
                 if (board[index] != '?')
                 {
-                    while (true)
-                    {
-                        Console.WriteLine("Silly, you can't put it there! It's taken!");
-                        Console.WriteLine("Input column: (1,2,3)");
-                        column = int.Parse(Console.ReadLine());
-                        Console.WriteLine("Input row: (a, b, c)");
-                        row = char.Parse(Console.ReadLine());
-                        index = (int)row - '0' - '0';
-                        if (row == 'b') index += column;
-                        else if (row == 'c') index += (column + 2);
-                        else index = Math.Abs(index - column);
-                        if (board[index] == '?')
-                        {
-                            break;
-                        }
-                        else continue;
-                    }
-
-                    }
+                    TryAgain(cycle, board);
+                    continue;
+                }
                     board[index] = player;
                 
                     Console.Clear();
 
-                Console.WriteLine("  1   2   3");
-                Console.WriteLine("| {0} | {1} | {2} |  a", board[0], board[1], board[2]);
-                Console.WriteLine(" ------------");
-                Console.WriteLine("| {0} | {1} | {2} |  b", board[3], board[4], board[5]);
-                Console.WriteLine(" ------------");
-                Console.WriteLine("| {0} | {1} | {2} |  c", board[6], board[7], board[8]);
-                Console.WriteLine();
+                Board(board);
                 numOfPlays++;
+                cycle++;
 
-                if ((board[8] == board[4] && board[4] == board[0] && board[8] != '?' || board[6] == board[4] && board[4] == board[2] && board[2] != '?' ||
-                     board[8] == board[7] && board[7] == board[6] && board[8] != '?' || board[5] == board[4] && board[4] == board[3] && board[5] != '?' ||
-                     board[2] == board[1] && board[1] == board[0] && board[2] != '?' || board[0] == board[3] && board[3] == board[6] && board[0] != '?' ||
-                     board[1] == board[4] && board[4] == board[7] && board[1] != '?' || board[2] == board[5] && board[5] == board[8] && board[2] != '?'))
+                if (numOfPlays < 5) continue;
+
+                else if (IsVictory(board))
                     {
                     Console.WriteLine(player +" has won the game! Times played: "+(numOfPlays));
                     break;
                 }
-                if (numOfPlays == 9)
+                else if (numOfPlays == 9)
                 {
                     Console.WriteLine("It's a tie, noobs.");
                     break;
@@ -94,5 +88,33 @@ namespace TicTacToe
 
             }
         }
+        static bool IsVictory(char[] board)
+        {
+            
+            return ((board[8] == board[4] && board[4] == board[0] && board[8] != '?' || board[6] == board[4] && board[4] == board[2] && board[2] != '?' ||
+                     board[8] == board[7] && board[7] == board[6] && board[8] != '?' || board[5] == board[4] && board[4] == board[3] && board[5] != '?' ||
+                     board[2] == board[1] && board[1] == board[0] && board[2] != '?' || board[0] == board[3] && board[3] == board[6] && board[0] != '?' ||
+                     board[1] == board[4] && board[4] == board[7] && board[1] != '?' || board[2] == board[5] && board[5] == board[8] && board[2] != '?'));
+
+        }
+        static void Board(char[] board)
+        {
+            Console.WriteLine("  1   2   3");
+            Console.WriteLine("| {0} | {1} | {2} |  a", board[0], board[1], board[2]);
+            Console.WriteLine(" ------------");
+            Console.WriteLine("| {0} | {1} | {2} |  b", board[3], board[4], board[5]);
+            Console.WriteLine(" ------------");
+            Console.WriteLine("| {0} | {1} | {2} |  c", board[6], board[7], board[8]);
+            Console.WriteLine();
+
+        }
+        static void TryAgain(int cycle, char[] board)
+        {
+            Console.Clear();
+            Board(board);
+            Console.WriteLine("Think about your input.");
+            cycle = 0;
+        }
+
     }
 }
